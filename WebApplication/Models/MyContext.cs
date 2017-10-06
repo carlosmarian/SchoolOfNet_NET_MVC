4596f241ba0d376671fd762558c2908cc58c9c45
Carlos H. M <carlosmarian@gmail.com>
2017-10-05 21:24:20 -0300

    Configurando o entity

---
+++ WebApplication/Models/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class MyContext : DbContext
+    {
+        //Ao subir a aplicação ele irá mapear com a estrutura de banco.
+        public DbSet<Pessoa> Pessoa { get; set; }
+        // public DbSet<OutrasClasses> OutrasClasses { get; set; }
+
+    }
+}
